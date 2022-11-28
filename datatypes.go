9f5921882046016e00551ab5c074031b206bc0e6
Wanzi Xiao <xwz0611@outlook.com>
2022-11-28 16:12:58 -0500

    Update Multilane and NSDV

--- datatypes.go
+++ datatypes.go
@@ -8,7 +8,6 @@ const dedicatedLane = 0
 const p1 = 0.94
 const p2 = 0.5
 const p3 = 0.2
-const p4 = 0.7
 const cp1 = 0.8
 const cp2 = 0.5
 const cp3 = 0.1
@@ -28,5 +27,10 @@ type MultiRoad []Road
 const k = 2
 
 var safeSpaceMin = [maxSpeed + 1]int{1 * k, 5 * k, 10 * k, 15 * k, 20 * k, 25 * k, 30 * k, 35 * k, 40 * k, 45 * k, 50 * k}
+
+const k1 = 1
+
+var safeSpacSDVMin = [maxSpeed + 1]int{1 * k1, 2 * k1, 3 * k1, 4 * k1, 5 * k1, 6 * k1, 7 * k1, 8 * k1, 9 * k1, 10 * k1, 11 * k1}
+
 var safeSpaceMax = [maxSpeed + 1]int{10 * k, 15 * k, 20 * k, 25 * k, 30 * k, 35 * k, 40 * k, 45 * k, 50 * k, 55 * k, 60 * k}
 var safetraffic = [maxSpeed + 1]int{10 * k, 15 * k, 20 * k, 25 * k, 30 * k, 35 * k, 40 * k, 45 * k, 50 * k, 55 * k, 60 * k}
